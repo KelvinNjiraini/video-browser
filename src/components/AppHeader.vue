8555ffd36eef8a37864022545a80f22b9ef4cd34
Kelvin Njiraini <njirainikelvin@gmail.com>
2021-03-02 14:19:32 +0300

    appheader functionality

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -4,7 +4,14 @@
             Image Storage
         </a>   
         <div class="right menu">
-            <a href="#" class="ui item" @click="login">
+            <div v-if="isLoggedIn" class="horizontal">
+                <a class="item" >Galleries</a>
+                <a class="item" >Upload</a>
+                <a class="item" >Logout</a>
+            </div>
+
+
+            <a v-else href="#" class="ui item" @click="login">
                 Login
             </a>
         </div>
@@ -12,10 +19,18 @@
 </template>
 
 <script>
-import { mapActions } from 'vuex';
+import { mapActions, mapGetters } from 'vuex';
 
 export default {
     name: 'AppHeader',
-    methods: mapActions(['login'])
+    computed: mapGetters(['isLoggedIn']),
+    methods: mapActions(['login', 'logout'])
 }
 </script>
+
+<style scoped>
+    .horizontal {
+        display: flex;
+        flex-direction: row;
+    }
+</style>
